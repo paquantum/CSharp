67039c6dd135dd629c64d072b2a9f8e802154daf
Park Minwoo <enmw34@naver.com>
2021-06-26 21:28:41 +0900

    add about Class
    CuteCat Class를 예시로 Class 공부

---
+++ CuteCat/Cat.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CuteCat
+{
+    public class Cat // 접근제한자가 없는경우 default = internal -> 이는 CuteCat 네임스페이스 툴박스에서만 사용 가능
+    {
+        public string Name;
+        public int Age;
+        private int Happiness = 50;
+
+        public Cat(string name, int age) // 외부에서 생성자 이용 못하면 의미가 없어 public로
+        {
+            this.Name = name;
+            this.Age = age;
+        }
+
+        public void GetBored()
+        {
+            Happiness -= 10;
+            if (Happiness < 0)
+                Happiness = 0;
+        }
+
+        public void Play()
+        {
+            Happiness += 10;
+            if (Happiness > 100)
+                Happiness = 100;
+        }
+
+        public void Eat()
+        {
+            Happiness += 20;
+            if (Happiness > 100)
+                Happiness = 100;
+        }
+
+        public string Express()
+        {
+            string message = "";
+            if (Happiness >= 80)
+                message = "I`m very happy";
+            else if (Happiness >= 60)
+                message = "I`m happy";
+            else if (Happiness >= 40)
+                message = "I`m so so";
+            else if (Happiness >= 20)
+                message = "I`m gloomy";
+            else
+                message = "I`m sad";
+
+            return this.Name + ": " + message;
+        }
+    }
+}
---
+++ CuteCat/Form1.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CuteCat
+{
+    public partial class Form1 : Form
+    {
+        private Cat MyCat = new Cat("Lucas", 1);
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Play_Click(object sender, EventArgs e)
+        {
+            MyCat.Play();
+            CatScreen.Text = MyCat.Express();
+        }
+
+        private void Feed_Click(object sender, EventArgs e)
+        {
+            MyCat.Eat();
+            CatScreen.Text = MyCat.Express();
+        }
+
+        private void CatTimer_Tick(object sender, EventArgs e)
+        {
+            // 5000 으로 설정해놔서 5초마다 이 함수가 실행 됨
+            MyCat.GetBored();
+            CatScreen.Text = MyCat.Express();
+        }
+    }
+}
