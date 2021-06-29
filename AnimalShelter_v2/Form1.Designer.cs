aaa2ada64ad623b89e9cd7425117163a48e84a87
Park Minwoo <enmw34@naver.com>
2021-06-29 20:19:26 +0900

    Collections, ArrayList, List, foreach
    Collections와 foreach의 사용

---
+++ AnimalShelter_v2/Form1.Designer.cs
@@ -0,0 +1,332 @@
+﻿
+namespace AnimalShelter
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 필수 디자이너 변수입니다.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 사용 중인 모든 리소스를 정리합니다.
+        /// </summary>
+        /// <param name="disposing">관리되는 리소스를 삭제해야 하면 true이고, 그렇지 않으면 false입니다.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 디자이너에서 생성한 코드
+
+        /// <summary>
+        /// 디자이너 지원에 필요한 메서드입니다. 
+        /// 이 메서드의 내용을 코드 편집기로 수정하지 마세요.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.CreateCustomer = new System.Windows.Forms.Button();
+            this.CusDetailPanel = new System.Windows.Forms.Panel();
+            this.CusIsQualified = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.CusDescription = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.CusAddress = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.CusAge = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.CusFullName = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.CusNewPanel = new System.Windows.Forms.Panel();
+            this.CusNewDescription = new System.Windows.Forms.TextBox();
+            this.CusNewAddress = new System.Windows.Forms.TextBox();
+            this.CusNewBirthday = new System.Windows.Forms.TextBox();
+            this.CusNewFirstName = new System.Windows.Forms.TextBox();
+            this.CusNewLastName = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label10 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label9 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.CustomerList = new System.Windows.Forms.ListBox();
+            this.CusDetailPanel.SuspendLayout();
+            this.CusNewPanel.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // CreateCustomer
+            // 
+            this.CreateCustomer.Location = new System.Drawing.Point(167, 338);
+            this.CreateCustomer.Name = "CreateCustomer";
+            this.CreateCustomer.Size = new System.Drawing.Size(132, 33);
+            this.CreateCustomer.TabIndex = 0;
+            this.CreateCustomer.Text = "입양자 생성";
+            this.CreateCustomer.UseVisualStyleBackColor = true;
+            this.CreateCustomer.Click += new System.EventHandler(this.CreateCustomer_Click);
+            // 
+            // CusDetailPanel
+            // 
+            this.CusDetailPanel.Controls.Add(this.CusIsQualified);
+            this.CusDetailPanel.Controls.Add(this.label2);
+            this.CusDetailPanel.Controls.Add(this.CusDescription);
+            this.CusDetailPanel.Controls.Add(this.label6);
+            this.CusDetailPanel.Controls.Add(this.CusAddress);
+            this.CusDetailPanel.Controls.Add(this.label5);
+            this.CusDetailPanel.Controls.Add(this.CusAge);
+            this.CusDetailPanel.Controls.Add(this.label4);
+            this.CusDetailPanel.Controls.Add(this.CusFullName);
+            this.CusDetailPanel.Controls.Add(this.label3);
+            this.CusDetailPanel.Location = new System.Drawing.Point(437, 20);
+            this.CusDetailPanel.Name = "CusDetailPanel";
+            this.CusDetailPanel.Size = new System.Drawing.Size(326, 389);
+            this.CusDetailPanel.TabIndex = 1;
+            // 
+            // CusIsQualified
+            // 
+            this.CusIsQualified.AutoSize = true;
+            this.CusIsQualified.Location = new System.Drawing.Point(95, 69);
+            this.CusIsQualified.Name = "CusIsQualified";
+            this.CusIsQualified.Size = new System.Drawing.Size(38, 12);
+            this.CusIsQualified.TabIndex = 11;
+            this.CusIsQualified.Text = "label2";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(22, 69);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(63, 12);
+            this.label2.TabIndex = 10;
+            this.label2.Text = "입양가능?:";
+            // 
+            // CusDescription
+            // 
+            this.CusDescription.AutoSize = true;
+            this.CusDescription.Location = new System.Drawing.Point(95, 120);
+            this.CusDescription.Name = "CusDescription";
+            this.CusDescription.Size = new System.Drawing.Size(38, 12);
+            this.CusDescription.TabIndex = 9;
+            this.CusDescription.Text = "label2";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(22, 120);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(33, 12);
+            this.label6.TabIndex = 8;
+            this.label6.Text = "설명:";
+            // 
+            // CusAddress
+            // 
+            this.CusAddress.AutoSize = true;
+            this.CusAddress.Location = new System.Drawing.Point(95, 94);
+            this.CusAddress.Name = "CusAddress";
+            this.CusAddress.Size = new System.Drawing.Size(38, 12);
+            this.CusAddress.TabIndex = 7;
+            this.CusAddress.Text = "label2";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(22, 94);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(33, 12);
+            this.label5.TabIndex = 6;
+            this.label5.Text = "주소:";
+            // 
+            // CusAge
+            // 
+            this.CusAge.AutoSize = true;
+            this.CusAge.Location = new System.Drawing.Point(95, 45);
+            this.CusAge.Name = "CusAge";
+            this.CusAge.Size = new System.Drawing.Size(38, 12);
+            this.CusAge.TabIndex = 5;
+            this.CusAge.Text = "label2";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(22, 45);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(33, 12);
+            this.label4.TabIndex = 4;
+            this.label4.Text = "나이:";
+            // 
+            // CusFullName
+            // 
+            this.CusFullName.AutoSize = true;
+            this.CusFullName.Location = new System.Drawing.Point(95, 18);
+            this.CusFullName.Name = "CusFullName";
+            this.CusFullName.Size = new System.Drawing.Size(38, 12);
+            this.CusFullName.TabIndex = 3;
+            this.CusFullName.Text = "label2";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(22, 18);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(33, 12);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "이름:";
+            // 
+            // CusNewPanel
+            // 
+            this.CusNewPanel.Controls.Add(this.CusNewDescription);
+            this.CusNewPanel.Controls.Add(this.CusNewAddress);
+            this.CusNewPanel.Controls.Add(this.CreateCustomer);
+            this.CusNewPanel.Controls.Add(this.CusNewBirthday);
+            this.CusNewPanel.Controls.Add(this.CusNewFirstName);
+            this.CusNewPanel.Controls.Add(this.CusNewLastName);
+            this.CusNewPanel.Controls.Add(this.label1);
+            this.CusNewPanel.Controls.Add(this.label10);
+            this.CusNewPanel.Controls.Add(this.label7);
+            this.CusNewPanel.Controls.Add(this.label9);
+            this.CusNewPanel.Controls.Add(this.label8);
+            this.CusNewPanel.Location = new System.Drawing.Point(132, 20);
+            this.CusNewPanel.Name = "CusNewPanel";
+            this.CusNewPanel.Size = new System.Drawing.Size(306, 389);
+            this.CusNewPanel.TabIndex = 2;
+            // 
+            // CusNewDescription
+            // 
+            this.CusNewDescription.Location = new System.Drawing.Point(69, 121);
+            this.CusNewDescription.Multiline = true;
+            this.CusNewDescription.Name = "CusNewDescription";
+            this.CusNewDescription.Size = new System.Drawing.Size(230, 207);
+            this.CusNewDescription.TabIndex = 21;
+            // 
+            // CusNewAddress
+            // 
+            this.CusNewAddress.Location = new System.Drawing.Point(69, 94);
+            this.CusNewAddress.Name = "CusNewAddress";
+            this.CusNewAddress.Size = new System.Drawing.Size(230, 21);
+            this.CusNewAddress.TabIndex = 20;
+            // 
+            // CusNewBirthday
+            // 
+            this.CusNewBirthday.Location = new System.Drawing.Point(69, 67);
+            this.CusNewBirthday.Name = "CusNewBirthday";
+            this.CusNewBirthday.Size = new System.Drawing.Size(100, 21);
+            this.CusNewBirthday.TabIndex = 19;
+            // 
+            // CusNewFirstName
+            // 
+            this.CusNewFirstName.Location = new System.Drawing.Point(69, 41);
+            this.CusNewFirstName.Name = "CusNewFirstName";
+            this.CusNewFirstName.Size = new System.Drawing.Size(100, 21);
+            this.CusNewFirstName.TabIndex = 18;
+            // 
+            // CusNewLastName
+            // 
+            this.CusNewLastName.Location = new System.Drawing.Point(69, 16);
+            this.CusNewLastName.Name = "CusNewLastName";
+            this.CusNewLastName.Size = new System.Drawing.Size(100, 21);
+            this.CusNewLastName.TabIndex = 17;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(17, 73);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(33, 12);
+            this.label1.TabIndex = 16;
+            this.label1.Text = "생일:";
+            // 
+            // label10
+            // 
+            this.label10.AutoSize = true;
+            this.label10.Location = new System.Drawing.Point(17, 19);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(21, 12);
+            this.label10.TabIndex = 12;
+            this.label10.Text = "성:";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(17, 125);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(33, 12);
+            this.label7.TabIndex = 15;
+            this.label7.Text = "설명:";
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Location = new System.Drawing.Point(17, 47);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(33, 12);
+            this.label9.TabIndex = 13;
+            this.label9.Text = "이름:";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(17, 99);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(33, 12);
+            this.label8.TabIndex = 14;
+            this.label8.Text = "주소:";
+            // 
+            // CustomerList
+            // 
+            this.CustomerList.FormattingEnabled = true;
+            this.CustomerList.ItemHeight = 12;
+            this.CustomerList.Location = new System.Drawing.Point(4, 13);
+            this.CustomerList.Name = "CustomerList";
+            this.CustomerList.Size = new System.Drawing.Size(122, 208);
+            this.CustomerList.TabIndex = 3;
+            this.CustomerList.Click += new System.EventHandler(this.CustomerList_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(793, 472);
+            this.Controls.Add(this.CustomerList);
+            this.Controls.Add(this.CusNewPanel);
+            this.Controls.Add(this.CusDetailPanel);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.CusDetailPanel.ResumeLayout(false);
+            this.CusDetailPanel.PerformLayout();
+            this.CusNewPanel.ResumeLayout(false);
+            this.CusNewPanel.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button CreateCustomer;
+        private System.Windows.Forms.Panel CusDetailPanel;
+        private System.Windows.Forms.Label CusFullName;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label CusDescription;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label CusAddress;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label CusAge;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label CusIsQualified;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Panel CusNewPanel;
+        private System.Windows.Forms.TextBox CusNewDescription;
+        private System.Windows.Forms.TextBox CusNewAddress;
+        private System.Windows.Forms.TextBox CusNewBirthday;
+        private System.Windows.Forms.TextBox CusNewFirstName;
+        private System.Windows.Forms.TextBox CusNewLastName;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label10;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.ListBox CustomerList;
+    }
+}
+
---
+++ AnimalShelter_v2/Form1.cs
@@ -0,0 +1,113 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Collections;
+
+namespace AnimalShelter
+{
+    public partial class Form1 : Form
+    {
+        // @배열을 컬렉션으로 교체
+        public List<Customer> Customers = new List<Customer>();
+        //public Customer[] CustomerArray = new Customer[10];
+        //public int CustomerArrayIndex = 0;
+        
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void CreateCustomer_Click(object sender, EventArgs e)
+        {
+            /* 배열 선언 방법들
+             * int[] numberArray = new int[5];
+             * numberArray[0] = 1;
+             * numberArray[1] = 2;
+             * int[] numberArray2 = new int[] {1, 2, 3, 4, 5};
+             * int[] numberArray3 = {1, 2, 3, 4, 5};
+            */
+            // @ 컬렉션 방식으로 교체
+            //CustomerArray[CustomerArrayIndex] = new Customer(CusNewFirstName.Text,
+            //    CusNewLastName.Text, DateTime.Parse(CusNewBirthday.Text));
+            //CustomerArray[CustomerArrayIndex].Address = CusNewAddress.Text;
+            //CustomerArray[CustomerArrayIndex].Description = CusNewDescription.Text;
+            //CustomerList.Items.Add(CustomerArray[CustomerArrayIndex].FirstName);
+            //CustomerArrayIndex += 1;
+            Customer cus = new Customer(CusNewFirstName.Text, 
+                CusNewLastName.Text, DateTime.Parse(CusNewBirthday.Text));
+            cus.Address = CusNewAddress.Text;
+            cus.Description = CusNewDescription.Text;
+            CustomerList.Items.Add(cus.FirstName);
+            Customers.Add(cus);
+        }
+        public void ShowDetails(Customer cus)
+        {
+            CusFullName.Text = cus.FullName;
+            CusAge.Text = cus.Age.ToString();
+            CusAddress.Text = cus.Address;
+            CusDescription.Text = cus.Description;
+            CusIsQualified.Text = cus.IsQualified.ToString();
+        }
+
+        private void CustomerList_Click(object sender, EventArgs e)
+        {
+            string firstName = CustomerList.SelectedItem.ToString();
+
+            // foreach로 교체
+            foreach (Customer cus in Customers)
+            {
+                if (cus.FirstName == firstName)
+                {
+                    ShowDetails(cus);
+                    break;
+                }
+            }
+            /*
+            for (int index = 0; index < CustomerArrayIndex; index++)
+            {
+                if (CustomerArray[index].FirstName == firstName)
+                {
+                    ShowDetails(CustomerArray[index]);
+                    break;
+                }
+            }*/
+        }
+
+        public void test()
+        {
+            // object를 받기 때문에 어떤 데이터 타입도 바을 수 있다
+            // 하지만 num 처럼 값을 받아올 때 형 변환이 필요하며
+            // 이는 데이터 양이 많아질수록 프로그램에 좋지 않다
+            ArrayList arrayList = new ArrayList();
+            arrayList.Add(1);
+            arrayList.Add("HI");
+            arrayList.Insert(2, 2);
+            arrayList.Remove(2);
+            arrayList.RemoveAt(1);
+            int num = (int)arrayList[0];
+
+            // List의 경우 데이터 타입을 지정해줌으로써 형변환이 생기지 않음
+            List<int> intList = new List<int>();
+            intList.Add(1);
+            intList.Add(2);
+            int sum2 = 0;
+            for (int index = 0; index < intList.Count; index++)
+            {
+                int value = intList[index];
+                sum2 += value;
+            }
+            // 위 아래 for foreach는 똑같은 역할을 함
+            // in 뒤에 컬렉션, 배열이 옴 foreach가 깔끔하고 효율적임
+            foreach (int value in intList)
+            {
+                sum2 += value;
+            }
+        }
+    }
+}
